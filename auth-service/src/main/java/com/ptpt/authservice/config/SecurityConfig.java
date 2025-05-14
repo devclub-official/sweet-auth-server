a75c5d5730e7cc33e167e98a251c8de0119c2270
cookiemiro <xodml9598@gmail.com>
2025-05-14 22:59:00 +0900

    fix: security config

--- auth-service/src/main/java/com/ptpt/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/ptpt/authservice/config/SecurityConfig.java
@@ -52,9 +52,8 @@ public class SecurityConfig {
         );
 
         http.authorizeHttpRequests(c -> {
-            c.requestMatchers("/v3/api-docs").permitAll();
             c.requestMatchers("/static/js/**","/static/css/**","/static/img/**"
-                    ,"/swagger-ui/**","/v3/api-docs/**").permitAll(); // swagger 관련 endpoint 허용
+                    ,"/swagger-ui/**","/v3/api-docs/**", "/v3/api-docs").permitAll(); // swagger 관련 endpoint 허용
             c.requestMatchers("/auth/**").permitAll();  // 인증 관련 엔드포인트 허용
             c.requestMatchers(HttpMethod.POST, "/api/users").permitAll();  // 회원가입만 허용
             c.anyRequest().authenticated();
